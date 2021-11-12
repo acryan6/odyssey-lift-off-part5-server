4e42a79bb3cf8fde994d3d717cc0440eb65448ec
Andrew Cryan <acryan6@gatech.edu>
2021-11-11 18:30:31 -0700

    Schema change

--- src/resolvers.js
+++ src/resolvers.js
@@ -35,6 +35,7 @@ const resolvers = {
         };
       }
     },
+    durationInSeconds: ({ length }) => length,
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
@@ -44,6 +45,9 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
